830000eeca7f363a545834d0ecd21f132024fd53
Jakob Poulsen <jakisp@gmail.com>
2026-06-03 16:16:49 +0200

    added login requirment for ledgers

--- controllers/ledger.py
+++ controllers/ledger.py
@@ -1,20 +1,26 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required, current_user
+
 from models.ledger import insert_ledger, list_ledgers
-from database import init_db, db_connection
+from database import db_connection
 
 bp = Blueprint('ledger', __name__, url_prefix='/ledgers')
 
 @bp.route('', methods=['GET', 'POST'])
+@login_required
 def ledgers():
     if request.method == 'POST':
         ledger_name = request.form['ledger_name']
-        insert_ledger(ledger_name)
+        insert_ledger(
+            user_id= current_user.id, 
+            ledger_name=ledger_name)
         return redirect(url_for('ledger.ledgers'))
 
-    ledgers = list_ledgers()
+    ledgers = list_ledgers(user_id= current_user.id)
     return render_template('ledgers.html', ledgers=ledgers)
 
 @bp.route('/<int:LedgerId>')
+@login_required
 def ledger(LedgerId):
     conn = db_connection()
 
