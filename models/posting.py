ea4e3a45e25b0ddb119238092b9b30e3d628a376
Christian Laustsen <pgh911@alumni.ku.dk>
2026-06-04 14:17:39 +0200

    Update posting.py
    update find category to use sql and update to CRUD

--- models/posting.py
+++ models/posting.py
@@ -1,45 +1,47 @@
 from database import db_connection
-from models.categories import Category, list_categories
 
 class Posting:
-    def __init__(self, pid, lid, cid, amount, description, category, posting_date):
+    def __init__(self, pid, lid, cid, amount, description, category_name, type_name, posting_date):
         self.pid = pid
         self.lid = lid
         self.cid = cid
-        self.category = category
         self.amount = amount
         self.description = description
+        self.category_name = category_name
+        self.type_name = type_name
         self.posting_date = posting_date
 
-def find_category(cid, category_list:list[Category]):
-    for CT in category_list:
-        if CT.cid == cid:
-            return CT.category_name
-    print("no match for category_id: " + str(cid) + "\nCategory length is: " + str(len(category_list)))
-    return cid
+def find_category(cid):
+    conn = db_connection()
+    db_category = conn.execute(
+        "SELECT category_name FROM categories WHERE cid = ?",
+        (cid,)
+    ).fetchone()
+    conn.close()
+
+    if db_category:
+        return db_category['category_name']
+    return None
 
 
 def list_postings(lid):
     conn = db_connection()
     db_postings = conn.execute(
-        "SELECT * FROM postings WHERE lid = ? ORDER BY pid",
+        "SELECT * FROM posting_details WHERE lid = ? ORDER BY pid",
         (lid,)
     ).fetchall()
     conn.close()
 
-    categories = list_categories(lid)
-
     postings = []
     for db_posting in db_postings:
-        category = find_category(db_posting['cid'], categories)
-
         postings.append(Posting(
             pid=db_posting['pid'],
             lid=db_posting['lid'],
             cid=db_posting['cid'],
-            category=category,
             amount=db_posting['amount'],
             description=db_posting['description'],
+            category_name=db_posting['category_name'],
+            type_name=db_posting['type_name'],
             posting_date=db_posting['posting_date']
         ))
     return postings
@@ -47,32 +49,29 @@ def list_postings(lid):
 def get_posting(pid):
     conn = db_connection()
     db_posting = conn.execute(
-        "SELECT * FROM postings WHERE pid = ?",
+        "SELECT * FROM posting_details WHERE pid = ?",
         (pid,)
     ).fetchone()
     conn.close()
 
-    categories = list_categories(db_posting['lid'])
-    
     if db_posting:
-        category = find_category(db_posting['cid'], categories)
-        
         return Posting(
             pid=db_posting['pid'],
             lid=db_posting['lid'],
             cid=db_posting['cid'],
-            category=category,
             amount=db_posting['amount'],
             description=db_posting['description'],
+            category_name=db_posting['category_name'],
+            type_name=db_posting['type_name'],
             posting_date=db_posting['posting_date']
         )
     return None
 
-def insert_posting(lid, cid, amount, description):
+def insert_posting(lid, cid, amount, description, posting_date):
     conn = db_connection()
     cur = conn.execute(
-        "INSERT INTO postings (lid, cid, amount, description) VALUES (?, ?, ?, ?)",
-        (lid, cid, amount, description)
+        "INSERT INTO postings (lid, cid, amount, description, posting_date) VALUES (?, ?, ?, ?, ?)",
+        (lid, cid, amount, description, posting_date)
     )
     conn.commit()
     posting_id = cur.lastrowid
@@ -90,6 +89,8 @@ def update_posting(pid, cid, amount, description):
 
 def delete_posting(pid):
     conn = db_connection()
-    conn.execute("DELETE FROM postings WHERE pid = ?", (pid,))
+    conn.execute("DELETE FROM postings WHERE pid = ?",
+                 (pid,)
+    )
     conn.commit()
     conn.close()
