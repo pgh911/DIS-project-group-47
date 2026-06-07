e4ebee1ed892d2efe290b11d3a6c694e5566db54
Jakob Poulsen <jakisp@gmail.com>
2026-06-07 21:33:46 +0200

    fikset fejl

--- database.py
+++ database.py
@@ -10,9 +10,9 @@ def init_db():
     conn = db_connection()
     with open("db/schema.sql") as f:
         conn.executescript(f.read())
-    with open("db/triggers.sql") as f:
-        conn.executescript(f.read())
     with open("db/seed.sql") as f:
+        conn.executescript(f.read())
+    with open("db/triggers.sql") as f:
         conn.executescript(f.read())
     with open("db/views.sql") as f:
         conn.executescript(f.read())
